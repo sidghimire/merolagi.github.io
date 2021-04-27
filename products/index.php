a0ac09848555f44eb12260b6842f5a9df585cf0b
Sid Ghimire <siddharthaghimire@gmail.com>
2021-04-27 15:57:17 +0545

    Upload 1

---
+++ products/index.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Bootstrap CSS -->
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.0.8/css/all.css"
+    />
+    <link href="/css/bootstrap.min.css" rel="stylesheet" />
+    <script src="/js/bootstrap.min.js"></script>
+    <link href="style.css" rel="stylesheet" />
+    <link href="/universalFiles/header.css" rel="stylesheet" />
+
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css"
+    />
+
+    <title>Merolagi.com</title>
+  </head>
+  <body>
+    <script src="/js/jquery.js"></script>
+
+    <?php
+    include $_SERVER['DOCUMENT_ROOT'].'/universalFiles/header.html';
+    ?>
+  <div class="container-fluid l-md-remove">
+    <div class=" d-flex row l-category-tab-div">
+      <div class="l-category-item col-md-1 text-center l-text-style-2">Artsy & Prints</div>
+      <div class="l-category-item col-md-1 text-center l-text-style-2">Stationary</div>
+      <div class="l-category-item col-md-1 text-center l-text-style-2">Tech</div>
+      <div class="l-category-item col-md-1 text-center l-text-style-2">Women</div>
+      <div class="l-category-item col-md-1 text-center l-text-style-2">Men</div>
+      <div class="l-category-item col-md-1 text-center l-text-style-2">Kids</div>
+      <div class="l-category-item col-md-1 text-center l-text-style-2">Food</div>
+      <div class="l-category-item col-md-1 text-center l-text-style-2">Go Local</div>
+      <div class="l-category-item col-md-1 text-center l-text-style-2">Fun</div>
+      <div class="l-category-item col-md-1 text-center l-text-style-2">Workshops</div>
+      <div class="l-category-item col-md-1 text-center l-text-style-2">Gifting</div>
+      <div class="l-category-item col-md-1 text-center l-text-style-2">Sales</div>
+    </div>
+  </div>
+  <div class="l-breadcrumb" style="padding:20px;padding-bottom:0">
+    <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">Home</li>
+        <li class="breadcrumb-item">Product</li>
+        <li class="breadcrumb-item active" aria-current="page">Large-Size-Sofa</li>
+      </ol>
+    </nav>
+  </div>
+  <div class="l-product-fixation-1">
+    <div class="row">
+      <div class="col-md-6 " style="display:flex;flex-direction:column">
+        <div class="">
+          <img src="/images/sofa1.jpg" class="w-100 l-big-image" alt="" srcset="">
+        </div>
+        <div class="row" style="margin-top: 20px;">
+          <div class="col-3" >
+            <img src="/images/sofa1.jpg" class="w-100 l-small-image" alt="" srcset="">
+          </div>
+          <div class="col-3">
+            <img src="/images/sofa2.jpg" class="w-100 l-small-image" alt="" srcset="">
+          </div>
+          <div class="col-3" >
+            <img src="/images/sofa3.jpeg" class="w-100 l-small-image" alt="" srcset="">
+          </div>
+          <div class="col-3" >
+            <img src="/images/sofa4.jpg" class="w-100 l-small-image" alt="" srcset="">
+          </div>
+          <div class="col-3" >
+            <img src="/images/sofa4.jpg" class="w-100 l-small-image" alt="" srcset="">
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6" style="padding-inline: 20px;">
+        <div class="w-100 product-description-main-div" style="display: flex;flex-direction:column">
+          <div class="l-description-title">
+            King Size Sofa For Adults
+          </div>
+          <div class="l-local-delivery">
+           <span class="bi-truck"></span> Free Local Delivery
+          </div>
+          <div class="l-price-and-share-div row" style="margin-top: 35px;">
+            <div class="col-md-6">
+              <div class="l-product-price">
+                Rs. 1600
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="l-social-media">
+                Share To : &emsp;
+                <i class="bi-facebook"></i>
+                <i class="bi-twitter"></i>
+                <i class="bi-whatsapp"></i>
+                <i class="bi-chat-dots"></i>
+                <i class="bi-linkedin"></i>
+                <i class="bi-envelope"></i>
+              </div>
+            </div>
+          </div>
+          <div class="l-quantity-div" style="display: flex;flex-direction:column">
+            <span class="l-label-1">Quantity</span>
+            <div class="d-flex" style="margin-top: 10px;">
+              <span class="bi-dash"></span>
+              <span class="quantity-counter">0</span>
+              <span class="bi-plus"></span>
+            </div>
+            <script src="script.js"></script>
+
+            <div class="d-flex" style="margin-top: 10px;">
+              <span class="color-choose color-blue"></span>
+              <span class="color-choose color-black"></span>
+              <span class="color-choose color-white"></span>
+              <span class="color-choose color-yellow"></span>
+              <span class="color-choose color-orange"></span>
+
+
+            </div>
+            <div class="addtocart-btn">Add To Cart</div>
+          </div>
+          <span class="l-label-1" style="margin-top: 10px;">Product Description <span class="bi-question-square"></span></span>
+
+          <div class="l-product-description">
+          A sectional sofa is modular. It usually includes at least two pieces that can be arranged in multiple configurations. A sectional sofa often has the option to choose the arm position as either left or right, with two or no arms. The key benefit of a sectional sofa is the ability to tailor its components to a range of spaces. Although sectional sofas were manufactured as early as the Victorian era, they did not become prominent until the mid-1950s, and they continue to be refined to the present day. At its most extreme, the Lava Sofa by Studio Vertijet -- a soft, flowing sectional -- can be arranged in any number of ways including crossed, paralleled or stacked.
+          </div>
+          <div class="additional-information " style="display: flex;flex-direction:column">
+            <span class="l-label-1">Additonal Information <span class="bi-question-square"></span></span>
+            <div class="row">
+              <div class="col-3" style="padding-left: 25px;padding-top:10px">
+                <span class="row l-additional-info-label">Material</span>
+                <span class="row l-additional-info-label">Size</span>
+
+              </div>
+              <div class="col-8" style="padding-top:10px">
+                <span class="row l-additional-info-label margin-30-res" style="color: black;">Wood</span>
+                <span class="row l-additional-info-label margin-30-res" style="color: black;">25*25</span>
+
+              </div>
+            </div>
+          </div>
+
+          <div class="additional-information " style="display: flex;flex-direction:column">
+            <span class="l-label-1">Shipping Information <span class="bi-truck"></span></span>
+            <div class="row">
+              <div class="row" style="padding-left: 25px;padding-top:10px">
+                <div class="col-3" >
+                  <span class="row l-additional-info-label">Estimated Arrival</span>
+                </div>
+                <div class="col-8">
+                  <span class="l-additional-info-label margin-30-res" style="color: black;">2-3 Days</span>
+                </div>
+              </div>
+              <div class="row" style="padding-left: 25px;padding-top:10px">
+                <div class="col-3" >
+                  <span class="row l-additional-info-label">Availability</span>
+                </div>
+                <div class="col-8">
+                  <span class="l-additional-info-label margin-30-res" style="color: black;">Kathmandu, Nepal</span>
+                </div>
+              </div>
+              <div class="row" style="padding-left: 25px;padding-top:10px">
+                <div class="col-3" >
+                  <span class="row l-additional-info-label">Return Policy</span>
+                </div>
+                <div class="col-8">
+                  <span class="l-additional-info-label margin-30-res" style="color: black;">Returnable</span>
+                </div>
+              </div>
+                
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </body>
+</html>
---
+++ retail/dashboard/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Bootstrap CSS -->
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.0.8/css/all.css"
+    />
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
+
+    <link href="/css/bootstrap.min.css" rel="stylesheet" />
+    <script src="/js/bootstrap.min.js"></script>
+    <link href="style.css" rel="stylesheet" />
+    <link href="/retail/dashboard/dashboardHeader/header.css" rel="stylesheet">
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css"
+    />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.1.0/chart.min.js"></script>
+    <title>Dashboard</title>
+  </head>
+  <body>
+    <?php
+      include $_SERVER['DOCUMENT_ROOT'].'/retail/dashboard/dashboardHeader/header.html';
+    ?>
+    <div class="w-100 l-tab-bar">
+      Welcome Back,Sid.
+    </div>
+    <div class="l-breadcrumb" style="padding:20px;padding-bottom:0">
+      <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item">Retailer Portal</li>
+          <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+        </ol>
+      </nav>
+    </div>
+    <div class="all-category-div " style="padding:25px;padding-top:5px">
+    <div class="row">
+        <div class="col-md-3 " style="padding: 20px;">
+          <div class="l-grid-border">
+            <canvas id="myChart" width="100" height="100">
+              <script src="script.js"></script>
+            </canvas>
+            <div class="l-dashboard-title text-center">
+              Categories You Trade
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 " style="padding: 20px;">
+          <div class="l-grid-border">
+            <canvas id="myChart2" width="100" height="100">
+              <script src="script.js"></script>
+            </canvas>
+            <div class="l-dashboard-title text-center">
+            Monthly Revenue
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  </body>
+</html>
